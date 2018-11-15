a3c7b8711f67a0cd68ef97f5aa17f1c928235da2
Amine Nokra <amine.nokra@gmail.com>
2018-11-15 16:35:39 +0100

    implicit intent

--- app/src/main/java/by/wink/jp3helloworld/WelcomeActivity.java
+++ app/src/main/java/by/wink/jp3helloworld/WelcomeActivity.java
@@ -1,16 +1,19 @@
 package by.wink.jp3helloworld;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     Intent intent;
     String email = "";
-
     TextView emailTv;
+    ImageButton sendMailBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,7 @@ public class WelcomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         emailTv = findViewById(R.id.email_tv);
+        sendMailBtn = findViewById(R.id.sendmail_btn);
 
         intent = getIntent();
         if(null!= intent && null!= intent.getExtras())
@@ -26,6 +30,16 @@ public class WelcomeActivity extends AppCompatActivity {
 
 
         emailTv.setText(email);
+        sendMailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("mailto:" + email);
+                Intent sendMailIntent = new Intent(Intent.ACTION_SENDTO,uri);
+
+                startActivity(sendMailIntent);
+            }
+        });
+
 
 
 
